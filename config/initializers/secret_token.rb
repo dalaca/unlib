498111ecb3f92b0a54be3c208b0f782aedeadf1e
Dave C <david1cardoso@gmail.com>
2013-09-03 23:19:38 -0400

    unlib framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unlib::Application.config.secret_key_base = '4ef7aa1d4c9e2d8816d1ca447cc2ed634e9918177886a72f8e71885c30bb4f6529a4c107134c5b80007eb87a99302bc80d8c97c27569f91da597062853aec52f'
