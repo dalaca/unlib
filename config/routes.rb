15069bb962cba09a04d0c21e8237107287c57355
Dave C <david1cardoso@gmail.com>
2013-09-18 21:23:32 -0400

    Added some UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Unlib::Application.routes.draw do
   root to: 'items#index'
   resources :items
+  resources :category, only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130915033427) do
+ActiveRecord::Schema.define(version: 20130918235955) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "items", force: true do |t|
     t.string  "title"
